b24b70707fcaa8c4d63460ff764f6ae0234c1ac8
IsabelaGomez101 <3379@holbertonschool.com>
2021-09-07 23:10:57 -0500

    adding file last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+ld = "Last digit of"
 n = number % 10
 if number < 0:
     n = (abs(number) % 10) * -1
 if n > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, n))
+    print("{} {} is {} and is greater than 5".format(ld, number, n))
 elif n == 0:
-	print("Last digit of {} is {} and is 0".format(number, n))
+    print("{} {} is {} and is 0".format(ld, number, n))
 else:
-	print("Last digit of {} is {} and is less than 6 and not 0".format(number, n))
+    print("{} {} is {} and is less than 6 and not 0".format(ld, number, n))
