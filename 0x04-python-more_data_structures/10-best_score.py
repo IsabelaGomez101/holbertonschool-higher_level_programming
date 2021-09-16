722236e26869a403d32e633bbf82da3b353615ce
IsabelaGomez101 <3379@holbertonschool.com>
2021-09-15 20:31:23 -0500

    adding file best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    if len(a_dictionary.values()) == 0:
+        return None
     nmax = max(a_dictionary.values())
     for key in a_dictionary:
         if a_dictionary[key] == nmax:
             return key
-    return None
