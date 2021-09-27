cdb0a3610d1f6c4e70bb12c8d30167c9ace1171c
IsabelaGomez101 <3379@holbertonschool.com>
2021-09-27 11:20:33 -0500

    adding file safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+ 
+def safe_print_list(my_list=[], x=0):
+        len = 0
+        try:
+            for i in range (0, x):
+                print("{}".format(my_list[i]), end="")
+                len += 1
+            print("")
+            return x
+        except:
+            print("")
+            return len
+         
