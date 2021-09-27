b11439fedfe38631e21b0759a8e6fb28b968b0e1
IsabelaGomez101 <3379@holbertonschool.com>
2021-09-27 11:48:18 -0500

    adding file print integer and correction pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
- 
+
+
 def safe_print_list(my_list=[], x=0):
         len = 0
         try:
-            for i in range (0, x):
+            for i in range(0, x):
                 print("{}".format(my_list[i]), end="")
                 len += 1
             print("")
@@ -11,4 +12,3 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             print("")
             return len
-         
