e44782f3f82ada659642cdb2cb3068a530fb0b62
IsabelaGomez101 <3379@holbertonschool.com>
2021-09-29 20:30:23 -0500

    correction file 1 rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """Private instance attribute width"""
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def height(self):
@@ -15,7 +15,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Define the method to get the height"""
-        if type(value) is not int:
+        if isinstance(value, int) is False:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -28,8 +28,8 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Define the method to get the heigth"""
-        if type(value) is not int:
+        """Define the method to get the width"""
+        if isinstance(value, int) is False:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
