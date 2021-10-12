e6792db580013fe6c07b1f978a0eba6d81242aad
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-11 22:44:25 -0500

    adding file square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class BaseGeometry"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class Square"""
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(self.__size, self.__size)
