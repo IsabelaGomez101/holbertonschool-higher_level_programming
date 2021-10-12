573dac061d3b6adf6259129c7bd9f29fdf50eb99
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-12 17:20:31 -0500

    adding file function write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function write_file """
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file
+    and returns the number of characters written"""
+    with open(filename, 'w') as f:
+        return f.write(text)
