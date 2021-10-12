76f5ffada450025aa0f8fd9660557c5419178435
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-12 17:47:45 -0500

    adding file to json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,7 +3,7 @@
 
 
 def to_json_string(my_obj):
-    import json
     """function that returns the JSON
     representation of an object (string):"""
-    return json.dumps(my_obj)
+    import json
+    return json.dumps(my_obj, sort_keys=True)
