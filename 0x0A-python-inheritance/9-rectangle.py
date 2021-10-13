62c5b2bffc128dda471cffb0004919066528267a
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-13 10:37:47 -0500

    correction code file 9 rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,27 +2,16 @@
 """ class BaseGeometry"""
 
 
-class BaseGeometry:
-    """class"""
-    def area(self):
-        """ Public instance method """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method, that validates value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """ class Rectangle that inherits from BaseGeometry """
     def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
 
     def area(self):
         return self.__width * self.__height
