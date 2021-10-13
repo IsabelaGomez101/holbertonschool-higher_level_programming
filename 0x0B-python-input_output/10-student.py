3caf58dee47ab29a33f7316ae3b9720f51c6a912
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-12 19:51:50 -0500

    adding file student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Class Student module"""
+
+
+class Student:
+    """ Student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ method that retrieves a dictionary representation of
+        a Student instance"""
+        if type(attrs) is list:
+            my_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    my_dict[i] = self.__dict__[i]
+            return my_dict
+            return attrs.__dict__
+        return self.__dict__
