77ab6574e60339af7bc2188bf151f0c8488f3789
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-18 16:09:45 -0500

    correction pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,12 +3,14 @@
 
 
 class Base:
-	""" """
-	__nb_objects = 0
+    """ Class Base
+    """
+    __nb_objects = 0
 
-	def __init__(self, id=None):
-		if id is not None:
-		    self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        """ initialize Base """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
