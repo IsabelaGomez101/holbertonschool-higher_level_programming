5eeeab8f8a9bcc87da361030bfeb7d5c4d44c4a9
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-19 12:05:16 -0500

    adding tests froms to json

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,19 +3,23 @@
 """
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 class TestBase(unittest.TestCase):
     def test_docstrings(self):
         self.assertIs(hasattr(Base, "__init__"), True)
         self.assertIs(hasattr(Base, "to_json_string"), True)
+        self.assertIs(hasattr(Base, "from_json_string"), True)
 
     def test_to_json_string(self):
         self.assertAlmostEqual(Base.to_json_string(None), "[]")
         self.assertAlmostEqual(Base.to_json_string([]), "[]")
         self.assertAlmostEqual(Base.to_json_string({'x': 2, 'width': 10}), '{"width": 10, "x": 2}')
+
+    def test_from_json_string(self):
+        self.assertAlmostEqual(Base.from_json_string(None), [])
+        self.assertAlmostEqual(Base.from_json_string("[]"), [])
+        self.assertAlmostEqual(Base.from_json_string('{"x": 2, "width": 10}'), {'width': 10, 'x': 2})
 
     def test_id(self):
         Base._Base__nb_objects = 0
@@ -29,11 +33,3 @@ class TestBase(unittest.TestCase):
         self.assertAlmostEqual(b3.id, 3)
         self.assertAlmostEqual(b4.id, 12)
         self.assertAlmostEqual(b5.id, 4)
-
-    def test_validate_attributes(self):
-        """ class Rectangle"""
-        r1 = Rectangle(10, 2)
-        r2 = Rectangle(2, 10)
-        self.assertAlmostEqual(r1.id, 5)
-        self.assertAlmostEqual(r2.id, 6)
-        self.assertRaises(TypeError, Rectangle, (2, "10"))
