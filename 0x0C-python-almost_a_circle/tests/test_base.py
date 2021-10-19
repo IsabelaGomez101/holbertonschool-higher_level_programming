09d95d8914a078c0ce481d0bd4750380f4dea03f
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-18 23:02:00 -0500

    add test

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,23 +3,33 @@
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-	def test_docstrings(self):
-		self.assertIs(hasattr(Base, "__init__"), True)
-		self.assertIs(hasattr(Base, "to_json_string"), True)
-		self.assertIs(hasattr(Base, "save_to_file"), True)
+    def test_docstrings(self):
+        self.assertIs(hasattr(Base, "__init__"), True)
+        self.assertIs(hasattr(Base, "to_json_string"), True)
+        self.assertIs(hasattr(Base, "save_to_file"), True)
 
-	def test_id(self):
-		Base._Base__nb_objects = 0
-		b1 = Base()
-		b2 = Base()
-		b3 = Base()
-		b4 = Base(12)
-		b5 = Base()
-		self.assertAlmostEqual(b1.id, 1)
-		self.assertAlmostEqual(b2.id, 2)
-		self.assertAlmostEqual(b3.id, 3)
-		self.assertAlmostEqual(b4.id, 12)
-		self.assertAlmostEqual(b5.id, 4)
+    def test_id(self):
+        Base._Base__nb_objects = 0
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(12)
+        b5 = Base()
+        self.assertAlmostEqual(b1.id, 1)
+        self.assertAlmostEqual(b2.id, 2)
+        self.assertAlmostEqual(b3.id, 3)
+        self.assertAlmostEqual(b4.id, 12)
+        self.assertAlmostEqual(b5.id, 4)
+
+    def test_validate_attributes(self):
+        """ class Rectangle"""
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        self.assertAlmostEqual(r1.id, 5)
+        self.assertAlmostEqual(r2.id, 6)
+       
