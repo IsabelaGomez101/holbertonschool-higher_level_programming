94d9d3516d6a70a6a1ff05756d7f3dcc7aa65426
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-19 14:05:54 -0500

    adding test class rectangle

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Unittest for class Rectangle
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestBase(unittest.TestCase):
+    def test_docstrings(self):
+        self.assertIs(hasattr(Rectangle, "__init__"), True)
+
+    def test_rectangle(self):
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(4, 6)
+        r2 = Rectangle(4, 5, 7)
+        r3 = Rectangle(1, 2, 3, 4)
+        self.assertAlmostEqual(r1.id, 1)
+        self.assertAlmostEqual(r2.id, 2)
+        self.assertAlmostEqual(r3.id, 3)
+
+    def test_validate_attribute(self):
+        self.assertRaises(TypeError, Rectangle, ("2", 10))
+        self.assertRaises(TypeError, Rectangle, (2, "10"))
+        self.assertRaises(TypeError, Rectangle, (2, 4, "10"))
+        self.assertRaises(TypeError, Rectangle, (2, 4, 6, "10"))
+        self.assertRaises(TypeError, Rectangle, (10, 2, 3, -1))
+        self.assertRaises(TypeError, Rectangle, (10, 2, 3, 4, 5))
