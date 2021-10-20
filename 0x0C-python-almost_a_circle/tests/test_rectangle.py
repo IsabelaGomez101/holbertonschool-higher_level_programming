2586aa84379214f63df2ec360bf19c86019e8c64
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-19 20:13:14 -0500

    adding tests rectangle

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -14,16 +14,11 @@ class TestBase(unittest.TestCase):
         self.assertIs(hasattr(Rectangle, "display"), True)
         self.assertIs(hasattr(Rectangle, "__str__"), True)
         self.assertIs(hasattr(Rectangle, "to_dictionary"), True)
-        self.assertIs(hasattr(Rectangle, "width"), True)
 
     def test_rectangle(self):
-        Base._Base__nb_objects = 0
-        r1 = Rectangle(4, 6)
-        r2 = Rectangle(4, 5, 7)
-        r3 = Rectangle(1, 2, 3, 4)
-        self.assertAlmostEqual(r1.id, 1)
-        self.assertAlmostEqual(r2.id, 2)
-        self.assertAlmostEqual(r3.id, 3)
+        self.assertIsInstance(Rectangle(2, 4), Rectangle)
+        self.assertIsInstance(Rectangle(4, 5, 7), Rectangle)
+        self.assertIsInstance(Rectangle(2, 4, 6, 7), Rectangle)
 
     def test_validate_attribute(self):
         self.assertRaises(TypeError, Rectangle, ("2", 10))
