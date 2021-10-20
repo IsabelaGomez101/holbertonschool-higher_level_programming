8bd1c0b45d1097db1aed761314a11a39f62054ae
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-19 21:23:04 -0500

    adding tests rectangle

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -16,13 +16,14 @@ class TestBase(unittest.TestCase):
         self.assertIs(hasattr(Rectangle, "to_dictionary"), True)
 
     def test_rectangle(self):
-        Base._Base__nb_objects = 0
-        r1 = Rectangle(1, 2)
-        r2 = Rectangle(1, 2, 3)
-        r3 = Rectangle(1, 2, 3, 4)
-        self.assertAlmostEqual(r1.id, 1)
-        self.assertAlmostEqual(r2.id, 2)
-        self.assertAlmostEqual(r3.id, 3)
+        if __name__ == "__main__":
+            Base._Base__nb_objects = 0
+            r1 = Rectangle(1, 2)
+            r2 = Rectangle(1, 2, 3)
+            r3 = Rectangle(1, 2, 3, 4)
+            self.assertAlmostEqual(r1.id, 1)
+            self.assertAlmostEqual(r2.id, 2)
+            self.assertAlmostEqual(r3.id, 3)
 
     def test_validate_attribute(self):
         self.assertRaises(TypeError, Rectangle, ("2", 10))
@@ -31,3 +32,9 @@ class TestBase(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, (2, 4, 6, "10"))
         self.assertRaises(TypeError, Rectangle, (10, 2, 3, -1))
         self.assertRaises(TypeError, Rectangle, (10, 2, 3, 4, 5))
+
+    def test_to_dictionary(self):
+        if __name__ == "__main__":
+            r1 = Rectangle(10, 2, 1, 9)
+            r1_dictionary = r1.to_dictionary()
+            self.assertAlmostEqual(r1_dictionary, {'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10})
