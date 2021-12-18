e139ebb511606103ffece150bd215321e4bee1ca
IsabelaGomez101 <isa.gomc@gmail.com>
2021-12-18 14:11:32 -0500

    addinf file 7 model state

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,7 +17,6 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    # HERE: no SQL query, only objects!
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
