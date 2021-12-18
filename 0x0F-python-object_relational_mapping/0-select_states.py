6edc001cc59060fa0767b4698469891ff17c9cde
IsabelaGomez101 <isa.gomc@gmail.com>
2021-12-18 12:44:34 -0500

    correction code file selec states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+
 import MySQLdb
 from sys import argv
-"""
+
+'''
 script that lists all states from the database hbtn_0e_0_usa
-"""
+'''
 
 if __name__ == '__main__':
     connect_db = MySQLdb.connect(host='localhost',
                                  port=3306,
                                  user=argv[1],
                                  password=argv[2],
-                                 database=argv[3])
+                                 database=argv[3],
+								 charset="utf8")
     cursor = connect_db.cursor()
-    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+    cursor.execute('SELECT * FROM states ORDER BY id ASC')
     data = cursor.fetchall()
     for state in data:
         print(state)
