c03a54594f4f3caa7c8f9ca0c767d418240fb979
IsabelaGomez101 <isa.gomc@gmail.com>
2022-01-13 18:00:10 -0500

    correction file hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
         html = response.read()
         print("Body response:\n\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf8')))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
