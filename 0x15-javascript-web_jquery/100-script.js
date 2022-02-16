9af7422a9701b0374967bc8b5a2f2684f1de40d9
IsabelaGomez101 <isa.gomc@gmail.com>
2022-02-15 21:42:03 -0500

    adding file 100 script

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// script that updates the text color of the <header> element to red.
+document.addEventListener('DOMContentLoaded', (event) => {
+  const headercolor = document.querySelector('header');
+  headercolor.style.color = '#FF0000';
+});
